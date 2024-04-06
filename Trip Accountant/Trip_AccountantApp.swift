d64a66100a7b43a3d3a2272d514c551526038d50
Paul Mac <plipp2804@gmail.com>
2024-04-06 11:38:40 -0700

    Initial Commit

---
+++ Trip Accountant/Trip_AccountantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trip_AccountantApp.swift
+//  Trip Accountant
+//
+//  Created by Paul Lipp on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Trip_AccountantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
